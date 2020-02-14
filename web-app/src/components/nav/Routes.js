93ffefbaeb0e056aa7ae9837127fc76df4ab4ced
Christian <christianoverton@ctoverton.com>
2020-02-13 23:40:05 -0500

    Added Playground for Research

--- web-app/src/components/nav/Routes.js
+++ web-app/src/components/nav/Routes.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Dashboard from "../screens/Dashboard";
 import {Switch, Route} from "react-router-dom";
+import SamTemplateComponent from "../../playground/sam/SamTemplateComponent";
+import SeanTemplateComponent from "../../playground/sean/SeanTemplateComponent";
 
 function Routes() {
     return(
         <Switch>
             <Route exact path='/' component={Dashboard} />
+
+            <Route exact path='/playground/sam' component={SamTemplateComponent} />
+            <Route exact path='/playground/sean' component={SeanTemplateComponent} />
+
             {/*<Route path='/project/:id' component={ProjectDetails} />
                 <Route exact path='/program/:id' component={ProgramDetails} />
                 <Route exact path='/program/:id/apply' component={Apply} />*/}
