f218aae3ce8f5dbdadf16e6ba253a59702920986
Christian <christianoverton@ctoverton.com>
2020-02-23 20:26:52 -0500

    API - Small Details

--- web-app/functions/routes/api.js
+++ web-app/functions/routes/api.js
@@ -26,11 +26,27 @@ router.get('/', function (req, res) {
     });
 });
 
+/*
+
+2020 LT2 Going
+GET lists/7f5d3fde05/segments/46905
+
+2020 LT3 Going
+GET lists/7f5d3fde05/segments/47813
+
+no-bus
+GET lists/7f5d3fde05/segments/47921
+
+GLT RSVP
+48005
+
+* */
+
 
 router.post('/sync/mailchimp', function (req, res) {
     let options = {
         'method': 'GET',
-        'url': mailchimpURL + 'lists/7f5d3fde05/segments/46373/members?count=100',
+        'url': mailchimpURL + 'lists/7f5d3fde05/segments/48005/members?count=100',
         'headers': {
             'Authorization': 'Bearer ' + apiKey
         }
@@ -38,23 +54,26 @@ router.post('/sync/mailchimp', function (req, res) {
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
         const data = JSON.parse(body)
-        res.send(200)
         data.members.forEach(member => {
             const attendee = {
                 id: member.id,
                 email: member.email_address,
                 firstName: member.merge_fields.FNAME,
                 lastName: member.merge_fields.LNAME,
+                phone: member.merge_fields.PNUMBER,
+                tags: ["2020 GLT RSVP"]
             }
 
             admin.firestore()
+                .collection('events')
+                .doc('n8HFZTwo9lHtdgOPKZT8')
                 .collection('attendees')
                 .doc(member.email_address)
                 .set(attendee)
                 .then(doc => console.log("attendee added with " + doc))
                 .catch(err => console.log(err))
-
         })
+        res.sendStatus(200)
     });
 });
 
