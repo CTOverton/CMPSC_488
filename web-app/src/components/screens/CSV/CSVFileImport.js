4c25df57a8ba3467808e7b79ba69862daca2d3f2
samuel <samuel.qtown@gmail.com>
2020-03-01 00:27:41 -0500

    csv_import -> fixed bug: Import works for static data

--- web-app/src/components/screens/CSV/CSVFileImport.js
+++ web-app/src/components/screens/CSV/CSVFileImport.js
@@ -3,7 +3,7 @@ import { CSVReader } from 'react-papaparse'
 import {Button, Table, TableHead} from "@material-ui/core";
 import Divider from "@material-ui/core/Divider";
 import {createAttendees} from "../../../redux/actions/eventActions";
-import connect from "react-redux/lib/connect/connect";
+import {connect} from "react-redux";
 
 
 class CSVFileImport extends Component {
@@ -20,8 +20,10 @@ class CSVFileImport extends Component {
     handleCreateAttendees = () => {
         this.props.dispatch(
             createAttendees(
-                [{email: "robocop72@hotmail.com", firstName: "Danny", lastName: "Denny", isUser: false}],
+                [{email: "robocop72@hotmail.com", firstName: "Danny", lastName: "Denny", isUser: false},
+                    {email: "jjb4@psu.edu", firstName: "Jeremy", lastName: "Blum", isUser: false}],
                 this.props.eventID))
+        console.log("COMPLETED");
     };
 
 
@@ -44,10 +46,11 @@ class CSVFileImport extends Component {
                     onError={this.handleOnError}
                 />
                 <Button variant="contained" disableElevation={true} onClick={this.handleImportOffer}>Import</Button>
-                <Button variant="contained" color = "primary" onClick={this.handleCreateAttendees}>Click Me!</Button>
             </>
         )
     }
 }
 
+
+//                <Button variant="contained" color = "primary" onClick={this.handleCreateAttendees}>Click Me!</Button>
 export default connect()(CSVFileImport)
