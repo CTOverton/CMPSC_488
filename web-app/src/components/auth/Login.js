6f80bc4adfd0b04705b00cdd3e347cd24e3cbce3
Christian <christianoverton@ctoverton.com>
2020-04-04 20:55:36 -0400

    Login and Signup components Remastered

--- web-app/src/components/auth/Login.js
+++ web-app/src/components/auth/Login.js
@@ -7,82 +7,90 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {isEmpty, isLoaded} from "react-redux-firebase";
-import {Link, Redirect} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
-    root: {
+    form: {
+        maxWidth: 400,
+        margin: 'auto',
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
             width: 300,
         },
     },
-    margin: {
-        margin: theme.spacing(1),
+    inputs: {
+        margin: theme.spacing(2),
     },
-}))
+    button: {
+        margin: theme.spacing(1),
+        width: '100%',
+        maxWidth: 300
+    }
+}));
 
 const Authorized = ({children}) => {
-    const auth = useSelector(state => state.firebase.auth)
+    const auth = useSelector(state => state.firebase.auth);
     if (!isLoaded(auth)) return null;
     if (!isEmpty(auth)) return <Redirect to='/' />;
     return children
-}
+};
 
-const Login = ({auth, login}) => {
+const Login = ({auth, login, history}) => {
     const classes = useStyles();
     const [inputs, setInputs] = React.useState({
         email: null,
         password: null
-    })
+    });
 
     const handleChange = prop => event => {
         const value = event.target.value;
         setInputs({ ...inputs, [prop]: value === "" ? null : value })
-    }
+    };
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         login({
             email: inputs.email,
             password: inputs.password
         })
-    }
+    };
 
     return(
         <Authorized>
             <Container maxWidth="md">
                 <h1>Login</h1>
-                <form className={classes.root} noValidate autoComplete="on" onSubmit={handleSubmit}>
-                    <TextField
-                        id="email-input"
-                        label="Email"
-                        type="email"
-                        autoComplete="email"
-                        variant="filled"
-                        onChange={handleChange('email')}
-                        required
-                    />
-                    <TextField
-                        id="password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        variant="filled"
-                        onChange={handleChange('password')}
-                        required
-                    />
-                    <div>
-                        <Link to={'/signup'} style={{ textDecoration: 'none' }}><Button className={classes.margin} variant="contained" disableElevation>Sign Up</Button></Link>
-                        <Button className={classes.margin} variant="contained" disableElevation color="primary" type="submit">Login</Button>
+                <form className={classes.form} noValidate autoComplete="on" onSubmit={handleSubmit}>
+                    <div className={classes.inputs}>
+                        <TextField
+                            id="email-input"
+                            label="Email"
+                            type="email"
+                            autoComplete="email"
+                            variant="filled"
+                            onChange={handleChange('email')}
+                            required
+                        />
+                        <TextField
+                            id="password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="filled"
+                            onChange={handleChange('password')}
+                            required
+                        />
                     </div>
+
+                    <Button className={classes.button} variant="contained" disableElevation color="primary" type="submit">Login</Button>
+                    <Button className={classes.button} variant="contained" disableElevation onClick={() => {return history.push('/signup')}}>Sign Up</Button>
                 </form>
                 <Typography variant="subtitle1">{auth.authError}</Typography>
             </Container>
         </Authorized>
     )
-}
+};
 
-const mapState = state => {return {auth: state.auth}}
-const mapDispatch = {login: loginUser}
+const mapState = state => {return {auth: state.auth}};
+const mapDispatch = {login: loginUser};
 
 export default connect(mapState, mapDispatch)(Login)
--- web-app/src/components/auth/SignUp.js
+++ web-app/src/components/auth/SignUp.js
@@ -7,118 +7,111 @@ import TextField from "@material-ui/core/TextField";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Typography from "@material-ui/core/Typography";
 import {isEmpty, isLoaded} from "react-redux-firebase";
-import {Link, Redirect} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
-    root: {
+    form: {
+        maxWidth: 400,
+        margin: 'auto',
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
             width: 300,
         },
     },
-    margin: {
-        margin: theme.spacing(1),
+    inputs: {
+        margin: theme.spacing(2),
     },
-}))
+    button: {
+        margin: theme.spacing(1),
+        width: '100%',
+        maxWidth: 300
+    }
+}));
 
 const Authorized = ({children}) => {
-    const auth = useSelector(state => state.firebase.auth)
+    const auth = useSelector(state => state.firebase.auth);
     if (!isLoaded(auth)) return null;
     if (!isEmpty(auth)) return <Redirect to='/' />;
     return children
-}
+};
 
-const SignUp = ({auth, createUser}) => {
+const SignUp = ({auth, createUser, history}) => {
     const classes = useStyles();
     const [inputs, setInputs] = React.useState({
         email: null,
         password: null,
-        firstName: null,
-        lastName: null,
+        passwordConfirm: null,
         username: null
-    })
+    });
 
     const handleChange = prop => e => {
         const value = e.target.value;
         setInputs({ ...inputs, [prop]: value === "" ? null : value })
-    }
+    };
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const credentials = {
             email: inputs.email,
             password: inputs.password
-        }
+        };
 
         const profile = {
             email: inputs.email,
-            username: inputs.username,
-            firstName: inputs.firstName,
-            lastName: inputs.lastName
-        }
+            username: inputs.username
+        };
 
         createUser(credentials, profile)
-    }
+    };
 
     return(
         <Authorized>
             <Container maxWidth="md">
                 <h1>Sign Up</h1>
-                <form className={classes.root} noValidate autoComplete="on" onSubmit={handleSubmit}>
-                    <TextField
-                        id="firstName-input"
-                        label="First Name"
-                        autoComplete="given-name"
-                        variant="filled"
-                        onChange={handleChange('firstName')}
-                    />
-                    <TextField
-                        id="lastName-input"
-                        label="Last Name"
-                        autoComplete="family-name"
-                        variant="filled"
-                        onChange={handleChange('lastName')}
-                    />
-                    <TextField
-                        id="username-input"
-                        label="Username"
-                        autoComplete="username"
-                        variant="filled"
-                        onChange={handleChange('username')}
-                        required
-                    />
-                    <TextField
-                        id="email-input"
-                        label="Email"
-                        type="email"
-                        autoComplete="email"
-                        variant="filled"
-                        onChange={handleChange('email')}
-                        required
-                    />
-                    <TextField
-                        id="password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        variant="filled"
-                        onChange={handleChange('password')}
-                        required
-                    />
-                    {/*                <TextField
-                    id="confirm-password-input"
-                    label="Confirm Password"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="filled"
-                    onChange={handleChange('password')}
-                    required
-                />*/}
-                    <div>
-                        <Link to={'/login'} style={{ textDecoration: 'none' }}><Button className={classes.margin} variant="contained" disableElevation>Login</Button></Link>
-                        <Button className={classes.margin} variant="contained" disableElevation color="primary" type="submit">Sign Up</Button>
+                <form className={classes.form} noValidate autoComplete="on" onSubmit={handleSubmit}>
+                    <div className={classes.inputs}>
+                        <TextField
+                            id="email-input"
+                            label="Email"
+                            type="email"
+                            autoComplete="email"
+                            variant="filled"
+                            onChange={handleChange('email')}
+                            required
+                        />
+                        <TextField
+                            id="password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="filled"
+                            onChange={handleChange('password')}
+                            required
+                        />
+                        {/* TODO: Confirm password */}
+                        {/*
+                        <TextField
+                            id="confirm-password-input"
+                            label="Confirm Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="filled"
+                            onChange={handleChange('passwordConfirm')}
+                            required
+                        />
+                        */}
+                        <TextField
+                            id="username-input"
+                            label="What should we call you?"
+                            autoComplete="name"
+                            variant="filled"
+                            onChange={handleChange('username')}
+                            required
+                        />
                     </div>
 
+                    <Button className={classes.button} variant="contained" disableElevation color="primary" type="submit">Sign Up</Button>
+                    <Button className={classes.button} variant="contained" disableElevation onClick={() => {return history.push('/login')}}>Login</Button>
 
                 </form>
                 <Typography variant="subtitle1">{auth.authError}</Typography>
@@ -126,9 +119,9 @@ const SignUp = ({auth, createUser}) => {
         </Authorized>
 
     )
-}
+};
 
-const mapState = state => {return {auth: state.auth}}
-const mapDispatch = {createUser: createUser}
+const mapState = state => {return {auth: state.auth}};
+const mapDispatch = {createUser: createUser};
 
 export default connect(mapState, mapDispatch)(SignUp)
